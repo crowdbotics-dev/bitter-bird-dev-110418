f1f8989a2b636035e82b94e958ab2557a9cfe049
Crowdbotics <engineering@crowdbotics.com>
2023-08-22 11:27:38 +0000

    Initial commit.

---
+++ backend/bitter_bird_dev_110418/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for bitter_bird_dev_110418 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bitter_bird_dev_110418.settings')
+
+application = get_wsgi_application()
